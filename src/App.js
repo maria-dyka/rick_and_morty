13ce06b00a5fc91b571640b00a8028447d097830
maria-dyka <mariadikaia21@gmail.com>
2020-01-11 15:38:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then(resp => {
+        console.log('characters', resp);
+      })
+      .catch(err => {
+        console.log('get characters error: ', err)
+      })
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="ListWrapper">
+      </div>    
     </div>
   );
 }
