71bbad69177d52327e3fffd217a01e55904e1d8d
maria-dyka <mariadikaia21@gmail.com>
2020-01-12 18:38:10 +0200

    simple pagination was added

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,7 +4,7 @@ import styles from './Banner.module.css'
 const Banner = props => {
     return (
         <div className={styles.Banner}>
-            <p className={styles.BannerName}>Search for character</p>
+            <p className={styles.BannerName}>Search for character on the page</p>
             <input
                 className={styles.BannerInput}
                 value={props.value}
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styles from './Home.module.css';
 
-import Banner from '../../components/Banner';
-import CharacterCard from '../../components/CharacterCard';
+import Banner from '../../components/Banner/Banner';
+import CharacterCard from '../../components/CharacterCard/CharacterCard';
 
-const Home = props => {
+const Home = () => {
     const [characters, setCharacters] = useState([]);
     const [searchText, setSearchText] = useState('');
     const [filteredCharacters, setFilteredCharacters] = useState([]);
+    const [pages, setPages] = useState({total: 1, current:1});
 
     const onChangeHandler = e => {
         setSearchText(e.target.value)
@@ -20,9 +21,23 @@ const Home = props => {
         }
     }
 
+    const onClickHandler = selected => {
+        axios.get(`https://rickandmortyapi.com/api/character/?page=${selected}`)
+        .then(resp => {
+            setPages({total: resp.data.info.pages, current: selected});
+            setCharacters(resp.data.results);
+            setFilteredCharacters(resp.data.results);
+            console.log(resp.data.results)
+        })
+        .catch(err => {
+            console.log('get characters error: ', err);
+        })
+    }
+
     useEffect(() => {
         axios.get('https://rickandmortyapi.com/api/character/')
         .then(resp => {
+            setPages({total: resp.data.info.pages, current: 1});
             setCharacters(resp.data.results);
             setFilteredCharacters(resp.data.results);
             console.log(resp.data.results)
@@ -32,9 +47,27 @@ const Home = props => {
         })
     }, [])
 
+    const PagesArray = new Array(pages.total).fill({});
+
     return (
         <>
             <Banner value={searchText} onChange={onChangeHandler}/>
+            <div className={styles.PagesWrapper}>
+                {PagesArray.map((page, index) => {
+                    if (index + 1 === pages.current) {
+                        return (
+                        <span
+                            key={index}
+                            className={styles.SelectedPage}
+                            onClick={() => onClickHandler(index + 1)}>{index + 1}</span>
+                        )
+                    }
+                return <span
+                        key={index}
+                        className={styles.Page} 
+                        onClick={() => onClickHandler(index + 1)}>{index + 1}</span>
+                })}
+            </div>
             <div className={styles.ListWrapper}>
                 {filteredCharacters.map(character => 
                 <CharacterCard 
@@ -43,7 +76,23 @@ const Home = props => {
                     name={character.name}
                     photo={character.image}
                     />)}
-            </div>  
+            </div>
+            <div className={styles.PagesWrapper}>
+                {PagesArray.map((page, index) => {
+                    if (index + 1 === pages.current) {
+                        return (
+                        <span
+                            key={index}
+                            className={styles.SelectedPage}
+                            onClick={() => onClickHandler(index + 1)}>{index + 1}</span>
+                        )
+                    }
+                return <span
+                        key={index}
+                        className={styles.Page} 
+                        onClick={() => onClickHandler(index + 1)}>{index + 1}</span>
+                })}
+            </div>
         </>
     );
 }
